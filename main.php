eff9d3528ec3813e56e1da48f91c96989764aac6
Chriss-hub136 <christiannegado1@gmail.com>
2025-08-13 01:32:43 +0000

    commit changes

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+    $name="Christian Joseph Negado";
+    $age="22";
+    $school="Global Reciprocal Colleges";
+    $course="BSIT";
+    $subject="Websys";
+    $student="Yes";
+
+    echo " Name: $name\n <br> ";
+    echo " age: $age \n <br> ";
+    echo " school: $school \n <br>";
+    echo " course: $course \n <br>";
+    echo " subject: $subject \n <br>";
+    echo "student:" , ($student ? "Yes" : "No") . "<br> \n<br>";
+
+?>
